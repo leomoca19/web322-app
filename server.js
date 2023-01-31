8e9bbe4e4b4c4e77a8eb2cf6953193632a6560d5
leomoca19 <leomoca19@gmail.com>
2023-01-31 08:56:49 -0500

    Adding Static Content

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-//comment
+var path = require("path");
+
+var express = require("express");
+var app = express();
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+// call this function after the http server starts listening for requests
+function onHttpStart() {
+  console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+// setup a 'route' to listen on the default url path (http://localhost)
+app.get("/", function(req,res){
+    //res.send("");
+});
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT, onHttpStart);
